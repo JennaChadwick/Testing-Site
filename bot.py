de5a63f827185aa31cfbac2a8acfefe4c074ad40
JennaChadwick <chadwickjennak@gmail.com>
2020-11-02 15:29:30 -0700

    Added File Writing

--- bot.py
+++ bot.py
@@ -8,10 +8,19 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+
 def get_some_text():
     f = open('testText.txt', "r")
+    text = f.readlines()
+    f.close
+    return text
+
+
+def write_some_text():
+    f = open('testText.txt', "a")
+    f.write("Extra Line")
     f.close()
-    return f
+
 
 client= commands.Bot(command_prefix = '.')
 
@@ -21,11 +30,15 @@ async def on_ready():
 
 @client.command()
 async def respond(ctx):
-    await ctx.send('hello #8')
+    await ctx.send('hello from the other side')
 
 @client.command()
 async def test(ctx):
     await ctx.send(get_some_text())
+
+@client.command()
+async def testWrite(ctx):
+    write_some_text()
 
 
 client.run(TOKEN)
