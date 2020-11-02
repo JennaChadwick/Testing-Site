f9ce84b9dbe3b627e8013ecda3317f489b6b02c0
Jenna Chadwick <70163429+JennaChadwick@users.noreply.github.com>
2020-11-02 14:46:02 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,16 @@
-import discord
-from discord.ext import commands
-
-# Imports
-from dotenv import load_dotenv
+# bot.py
 import os
 
-# Credentials
-load_dotenv('.env')
+import discord
+from dotenv import load_dotenv
 
-def get_some_text():
-    f = open('testText.txt', "r")
-    f.close()
-    return f
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
-client= commands.Bot(command_prefix = '.')
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Bot is ready. ')
+    print(f'{client.user} has connected to Discord!')
 
-@client.command()
-async def respond(ctx):
-    await ctx.send('hello #8')
-
-@client.command()
-async def test(ctx):
-    await ctx.send(get_some_text())
-
-client.run(os.getenv('My_Bot_Token'))
+client.run(TOKEN)
